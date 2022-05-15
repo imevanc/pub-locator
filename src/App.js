81279a21043565d593925713817f8ec1041e81a4
imevanc <charal.vag@gmail.com>
2022-05-15 20:06:44 +0100

    completed the map fitting between the header and the footer

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-import Map from "./Components/Map";
-import LinearWithColor from "./Components/LinearWithColor";
 import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import Styles from "./Themes/Styles";
 import Theme from "./Themes/Theme";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import { StylesContext } from "./Themes/StylesContext";
 import * as React from "react";
-import * as api from "./api";
+import MainFrame from "./Components/MainFrame";
 
 const App = () => {
   const [mode, setMode] = React.useState("light");
   const theme = Theme(mode);
   const styles = Styles();
-  const [pubLocations, setPubLocations] = React.useState([]);
-  const [imgURLs, setImgURLs] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchPubs = async () => {
-      return api
-        .getPubs()
-        .then((res) => {
-          setPubLocations(res);
-        })
-        .catch((error) => console.log(error));
-    };
-    fetchPubs();
-    const fetchImg = async () => {
-      return api
-        .getImg()
-        .then((res) => {
-          return res;
-        })
-        .then((fetchedImgs) => {
-          setImgURLs(fetchedImgs.map((img) => img.src.landscape));
-        })
-        .catch((error) => console.log(error));
-    };
-    fetchImg();
-  }, []);
 
   return (
     <StylesContext.Provider value={{ styles: styles.classic, theme: theme }}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Header mode={mode} setMode={setMode} />
-        {pubLocations.length && imgURLs.length ? (
-          <Map imgURLs={imgURLs} pubLocations={pubLocations} />
-        ) : (
-          <LinearWithColor />
-        )}
+        <MainFrame />
+
         <Footer />
       </ThemeProvider>
     </StylesContext.Provider>
